17e3ab10b5fd89014d06324b1e12529899b22492
Ten One Design <peter@tenonedesign.com>
2022-10-10 19:27:31 -0400

    - basic operation

--- Koinos Node/ContentView.swift
+++ Koinos Node/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Koinos Node
 //
-//  Created by Peter Skinner on 10/10/22.
+//  Created by Peter Skinner on 10/9/22.
 //
 
 import SwiftUI
@@ -15,68 +15,56 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    
+    let executableURL = URL(fileURLWithPath: "/bin/bash")
+    let containerDirectory = "\(FileManager.default.urls(for:.applicationSupportDirectory, in:.userDomainMask).first?.path ?? "")/Koinos Node/"
+    let producer_address = "15a6ZZR9M52Vt5ppW4iqoQEUfuNxrasgjF"
+    let miner_private_key = "5Ht9N1QNuPZ1aRMp9jh9fPM5apNjKBLCuPZL5BisCpswMSEz9Mn"
+    let version = "0.4"
 
+    @State var message = "Hello, World!"
+    @State var genericRunning = false
+    @State var nodeRunning = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            Text("Koinos node")
+                .font(.largeTitle)
+                .padding()
+            VStack {
+                Button(action: {
+                    let scriptPath = Bundle.main.path(forResource: "koinos-init", ofType: "sh") ?? ""
+                    self.genericRunning = true
+                    try! Process.run(executableURL,
+                                     arguments: ["-c", "source '\(scriptPath)' '\(containerDirectory)' '\(producer_address)' '\(miner_private_key)' '\(version)'"],
+                                     terminationHandler: { _ in self.genericRunning = false })
+                }) {
+                    Text("Full reset and init")
+                }.disabled(genericRunning).padding()
+                
+                Button(action: {
+                    self.nodeRunning = true
+                    try! Process.run(executableURL,
+                                     arguments: ["-c", "cd '\(containerDirectory)koinos_node/koinos';  /usr/local/bin/docker-compose-v1 --profile all up"],
+                                     terminationHandler: { _ in self.nodeRunning = false })
+                }) {
+                    Text("Start node")
+                }.disabled(nodeRunning).padding()
+                
+                Button(action: {
+                    self.genericRunning = true
+                    try! Process.run(executableURL,
+                                     arguments: ["-c", "cd '\(containerDirectory)koinos_node/koinos';  /usr/local/bin/docker-compose-v1 --profile all down"],
+                                     terminationHandler: { _ in self.genericRunning = false; self.nodeRunning = false })
+                }) {
+                    Text("Stop node")
+                }.disabled(genericRunning).padding()
+                
+                
             }
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
