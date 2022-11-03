f04566b47f39b799af67b05f2682c31c2bd8cfc6
Ten One Design <peter@tenonedesign.com>
2022-11-03 13:14:28 -0400

    - fixed koinos directory reference in reload script

--- Koinos Node/koinos-reload.sh
+++ Koinos Node/koinos-reload.sh
@@ -29,29 +29,29 @@ echo $KOINOS_MINER_PRIVATE_KEY > "$KOINOS_DATA_DIRECTORY/block_producer/private.
 
 sed -i.'' "s/# producer:/producer: $KOINOS_PRODUCER_ADDRESS/g" "$KOINOS_DIRECTORY/koinos/config/default-config.yml"
 
-sed -i '' "s|BASEDIR=~/.koinos|BASEDIR=$KOINOS_DATA_DIRECTORY|g" .env
+sed -i '' "s|BASEDIR=~/.koinos|BASEDIR=$KOINOS_DATA_DIRECTORY|g" "$KOINOS_DIRECTORY/koinos/.env"
 
 
 # Can use this strategy for between-release updates on particular images
 # This one updates the v0.4.0 release to use newer images
-sed -i '' 's/MEMPOOL_TAG=v0.4.0/MEMPOOL_TAG=v0.4.1/g' .env
-sed -i '' 's/CHAIN_TAG=v0.4.0/CHAIN_TAG=v0.4.2/g' .env
-sed -i '' 's/BLOCK_STORE_TAG=v0.4.0/BLOCK_STORE_TAG=v0.4.2/g' .env
-sed -i '' 's/P2P_TAG=v0.4.0/P2P_TAG=v0.4.1/g' .env
-sed -i '' 's/BLOCK_PRODUCER_TAG=v0.4.0/BLOCK_PRODUCER_TAG=v0.4.1/g' .env
-sed -i '' 's/TRANSACTION_STORE_TAG=v0.4.0/TRANSACTION_STORE_TAG=v0.4.2/g' .env
-sed -i '' 's/CONTRACT_META_STORE_TAG=v0.4.0/CONTRACT_META_STORE_TAG=v0.4.2/g' .env
-sed -i '' 's/JSONRPC_TAG=v0.4.0/JSONRPC_TAG=v0.4.2/g' .env
+sed -i '' 's/MEMPOOL_TAG=v0.4.0/MEMPOOL_TAG=v0.4.1/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/CHAIN_TAG=v0.4.0/CHAIN_TAG=v0.4.2/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/BLOCK_STORE_TAG=v0.4.0/BLOCK_STORE_TAG=v0.4.2/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/P2P_TAG=v0.4.0/P2P_TAG=v0.4.1/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/BLOCK_PRODUCER_TAG=v0.4.0/BLOCK_PRODUCER_TAG=v0.4.1/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/TRANSACTION_STORE_TAG=v0.4.0/TRANSACTION_STORE_TAG=v0.4.2/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/CONTRACT_META_STORE_TAG=v0.4.0/CONTRACT_META_STORE_TAG=v0.4.2/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/JSONRPC_TAG=v0.4.0/JSONRPC_TAG=v0.4.2/g' "$KOINOS_DIRECTORY/koinos/.env"
 
 # This one updates the v04.1 release to use newer images
-sed -i '' 's/MEMPOOL_TAG=v0.4.1//g' .env
-sed -i '' 's/CHAIN_TAG=v0.4.2/CHAIN_TAG=v0.4.3/g' .env
-sed -i '' 's/BLOCK_STORE_TAG=v0.4.2//g' .env
-sed -i '' 's/P2P_TAG=v0.4.2//g' .env
-sed -i '' 's/BLOCK_PRODUCER_TAG=v0.4.2//g' .env
-sed -i '' 's/TRANSACTION_STORE_TAG=v0.4.3//g' .env
-sed -i '' 's/CONTRACT_META_STORE_TAG=v0.4.3//g' .env
-sed -i '' 's/JSONRPC_TAG=v0.4.2//g' .env
+sed -i '' 's/MEMPOOL_TAG=v0.4.1//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/CHAIN_TAG=v0.4.2/CHAIN_TAG=v0.4.3/g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/BLOCK_STORE_TAG=v0.4.2//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/P2P_TAG=v0.4.2//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/BLOCK_PRODUCER_TAG=v0.4.2//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/TRANSACTION_STORE_TAG=v0.4.3//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/CONTRACT_META_STORE_TAG=v0.4.3//g' "$KOINOS_DIRECTORY/koinos/.env"
+sed -i '' 's/JSONRPC_TAG=v0.4.2//g' "$KOINOS_DIRECTORY/koinos/.env"
 
 # Support for Apple Silicon
 cd "$KOINOS_DIRECTORY/koinos/config"
